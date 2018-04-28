e80fa0faf2fb022457d5436ede4009448dbf57e7
Elaine Zhang <31319982+Beceelaine@users.noreply.github.com>
2018-04-28 00:09:56 -0400

    Update and rename _init_.py to __init__.py

---
+++ IBP/__init__.py
@@ -0,0 +1,4 @@
+from .gibbs import new_K
+from .gibbs import full_X
+from .gibbs import gibbs_sampler
+
--- IBP/_init_.py
+++
@@ -1,4 +0,0 @@
-from .IBP import new_K
-from .IBP import full_X
-from .IBP import gibbs_sampler
-
